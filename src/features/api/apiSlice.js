4a8df947c123a760040223fd929ba10e93310fe5
sumonbarai <sumonbarai78@gmail.com>
2023-01-30 12:07:41 +0600

    server name change

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:9000/",
+    baseUrl: "https://nervous-bass-pocketbook.cyclic.app",
     prepareHeaders: async (headers, { getState, endpoint }) => {
       const token = await getState().auth?.accessToken;
       if (token) {
